74e59d149902c13c16a106b75f35899340a15097
friendly <friendllyhulu@gmail.com>
2021-04-12 15:07:50 +0700

    update lagi

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 class TextScramble {
   constructor(el) {
     this.el = el
-    this.chars = '!<>-_\\/[]{}—=+*^?#________()^#!'
+    this.chars = '!<>-_\\/[]{}—=+*^?#_______'
     this.update = this.update.bind(this)
   }
   setText(newText) {
@@ -15,7 +15,12 @@ class TextScramble {
       const to = newText[i] || ''
       const start = Math.floor(Math.random() * 40)
       const end = start + Math.floor(Math.random() * 40)
-      this.queue.push({ from, to, start, end })
+      this.queue.push({
+        from,
+        to,
+        start,
+        end
+      })
     }
     cancelAnimationFrame(this.frameRequest)
     this.frame = 0
@@ -26,7 +31,13 @@ class TextScramble {
     let output = ''
     let complete = 0
     for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
+      let {
+        from,
+        to,
+        start,
+        end,
+        char
+      } = this.queue[i]
       if (this.frame >= end) {
         complete++
         output += to
